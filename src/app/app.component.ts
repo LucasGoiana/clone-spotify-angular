edf9de280d2930a75ac28176ecbdbb6df494f0cf
LucasGoianaMalicia <lucasgoianam@hotmail.com>
2022-03-09 16:22:47 -0300

    fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { Subscription } from 'rxjs';
-import { AlbunsService } from './services/albuns.service';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
